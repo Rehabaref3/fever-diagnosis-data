32193c5eacbfb0efe3c30aa348b1d30bef416aa3
noran243noran-pixel <noran243noran@gmail.com>
2026-04-13 02:11:45 +0300

    Add show_text function to display messages

--- nooran-visualization.py
+++ nooran-visualization.py
@@ -15,3 +15,8 @@ title.pack(pady=12)
 
 text_box = tk.Text(root, height=7, width=60)
 text_box.pack(pady=10)
+def show_text(msg):
+    text_box.delete("1.0", tk.END)
+    text_box.insert(tk.END, msg)
+    print(msg)
+    print("-" * 50)
